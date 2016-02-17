b165604b17078a2db104ec2f4e50cb7d118a8137
GucciTheCarpenter <stancutr@gmail.com>
2016-02-17 16:39:44 +0100

    octave begin

---
+++ ch02_Statistical_Learning/Intro.m
@@ -0,0 +1,18 @@
+# vector 
+x = [2, 7, 5]
+
+# vector w step
+y = 4:3:12
+
+# addition
+x + y
+
+# division, element by element
+x ./ y
+
+# exponent, element by element
+x .^ y
+
+# indexing (1-indexed)
+x(2)
+x(2:end)
